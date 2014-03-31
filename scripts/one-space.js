22595b5fe5de4dd3c47eb72ede00f7aa5ce1feaf
J.Y Han <ihanjiyun@gmail.com>
2014-04-01 07:36:16 +0800

    mo

--- scripts/one-space.js
+++ scripts/one-space.js
@@ -2,6 +2,20 @@
 
 $(function(){
 
+    if($.browser.msie) { 
+        alert("this is msie"); 
+    }
+
+    var _IE = (function(){
+        var v = 3,
+            div = document.createElement('div'),
+            all = div.getElementsByTagName('i');
+        while (
+            div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+            all[0]
+        );
+        return v > 4 ? v : false ;
+    }());
 
 // draw line
     var c_1 = document.getElementById("line_1");
